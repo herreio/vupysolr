d5bcf6727054d8f54434be510b71bb63e1340a79
Donatus Herre <donatus.herre@slub-dresden.de>
2022-06-19 14:25:24 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="vupysolr",
-    version="0.2.0",
+    version="0.2.1",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="Access Solr stored VuFind records.",
--- vupysolr/__init__.py
+++ vupysolr/__init__.py
@@ -6,7 +6,7 @@ https://vufind.org
 """
 
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .docs import VuFindParser
 from .base import VuFindIndex, VuFindCluster
